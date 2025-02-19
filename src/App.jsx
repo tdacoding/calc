480c4295c4f2f603cd0c87b6465d74eab8c6cf7d
Dmitry Timushev <timushev@gmail.com>
2025-02-19 12:05:01 +0300

    correcting

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,68 @@ export const App = () => {
 	const [operand1, setOperand1] = useState('');
 	const [operand2, setOperand2] = useState('');
 	const [operator, setOperator] = useState('');
+	const [resultedFlag, setResultedFlag] = useState(false);
+
+	const keyPressed = (key) => {
+		if (!Number.isNaN(+key)) {
+			!operator
+				? setOperand1((oper) => oper + key)
+				: setOperand2((oper) => oper + key);
+		} else {
+			operatorKeyPressed(key);
+		}
+	};
+
+	const operatorKeyPressed = (key) => {
+		if (key === 'C') {
+			setOperator('');
+			setOperand1('');
+			setOperand2('');
+			setResultedFlag(false);
+			return;
+		}
+		if (key === '+' && !operator) {
+			setOperator('+');
+			setResultedFlag(false);
+			return;
+		}
+		if (key === '-' && !operator) {
+			setOperator('-');
+			setResultedFlag(false);
+			return;
+		}
+		if (key === '=' && operator) {
+			const res = eval(operand1 + operator + operand2);
+			setResultedFlag(true);
+			setOperator('');
+			setOperand1(res);
+			setOperand2('');
+			return;
+		}
+	};
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.display}>
-				<span className={styles.spanDisplay}>0</span>
+				<span
+					className={
+						resultedFlag
+							? styles.spanDisplay + ' ' + styles.result
+							: styles.spanDisplay
+					}
+				>
+					{operand1 + operator + operand2}
+				</span>
 			</div>
 			<div className={styles['buttons-container']}>
-				{NUMS.map((key) => {
+				{NUMS.map((digit) => {
 					return (
-						<button className={styles.button} key={key}>
-							{key}
+						<button
+							className={styles.button}
+							key={digit}
+							onClick={() => keyPressed(digit)}
+						>
+							{digit}
 						</button>
 					);
 				})}
